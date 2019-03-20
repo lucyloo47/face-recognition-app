7974a539644042e93633f001fce36c9bacbe0471
alexandriatalley <alblythe47@gmail.com>
2019-03-20 14:38:15 -0600

    added heroku addres to fetch

--- src/Components/SignIn/signin.js
+++ src/Components/SignIn/signin.js
@@ -20,7 +20,7 @@ class SignIn extends Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://fast-headland-80560.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
